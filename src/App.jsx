e48defac1119b77a740a6570707e28dcf594f59c
floriansr <sueurfloriansf@gmail.com>
2020-05-06 11:09:56 +0200

    better starter

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "assets/scss/styles.scss";
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<>
+				<p>starter</p>
+			</>
+		);
+	}
+}
+
+export default App;
